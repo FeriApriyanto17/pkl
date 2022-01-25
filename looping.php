6eee40a0cfbeeec69c60ea3de20733512960ddad
FeriApriyanto17 <97430559+FeriApriyanto17@users.noreply.github.com>
2022-01-25 17:11:52 +0700

    Create looping.php

---
+++ looping.php
@@ -0,0 +1,10 @@
+<?php
+  $array=["nama"." ","saya"," ","Fery"];
+  foreach($array as $item){
+      if($item != "saya"){
+          echo $item;
+      }else{
+          echo ":";
+      }
+    }
+    ?>
